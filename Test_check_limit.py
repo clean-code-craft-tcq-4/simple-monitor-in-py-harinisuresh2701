44bb51254a203203490f6b376acd55e41d729677
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-07-05 18:56:25 +0530

    Create Test_check_limit.py

---
+++ Test_check_limit.py
@@ -0,0 +1,4 @@
+import check_limit
+assert(check_limit.battery_is_ok("Temperature", 25, 0, 45) 
+assert(check_limit.battery_is_ok("State of Charge", 70, 20, 80)
+assert(check_limit.battery_is_ok("Charge Rate", 0.7, 0.8, 1)
